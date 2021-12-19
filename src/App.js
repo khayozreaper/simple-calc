727c7ae35cd5f0ad766ad1b732439f1f6ed8ad4f
khayozreaper <69615896+khayozreaper@users.noreply.github.com>
2021-12-20 01:20:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Prorata from './Components/Prorata';
+import Discount from './Components/Discount';
 
 function App() {
   return (
     <div className="App">
-      <Prorata/>
+      <Discount />
     </div>
   );
 }
