fe4b540d9c3283afef89ba70557d5ca5705f74d4
khayozreaper <69615896+khayozreaper@users.noreply.github.com>
2022-01-01 02:10:44 +0530

    Initial commit

--- src/Components/Prorata.js
+++ src/Components/Prorata.js
@@ -1,47 +1,94 @@
 import { useState } from "react";
+
 import ReactDOM from "react-dom";
 import "./Prorata.css";
-
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 function Prorata() {
+
+  const [selectedDate, setSelecetedDate] = useState(null)
+  const [selectedDate2, setSelecetedDate2] = useState(new Date())
+  const [month, setMonth] = useState()
+
+
   const [number1, setNumber1] = useState();
-  const [number2, setNumber2] = useState();
   const [number3, setNumber3] = useState();
- 
+
   const [total, setTotal] = useState();
   const [discount, setDiscount] = useState();
 
   function calculateTotal() {
-    const number4 = Math.round((number2/number3 )*100)
-    setTotal(number1 - (number1 * (1-(number4)/100)));
-    setDiscount( Math.round((1-(number2/number3 ))*100));
+    const number2 = ((selectedDate2 - selectedDate) / ((100 * 60 * 60 * 24 * 30) * 10));
+    
+    const number4 = Math.round((number2 / number3) * 100)
+    setTotal(number1 - (number1 * (1 - (number4) / 100)));
+    setDiscount(Math.round((1 - (number2 / number3)) * 100));
+
+
+  }
+
+  function calculateMonth() {
+    setMonth(Math.round((selectedDate2 - selectedDate) / ((100 * 60 * 60 * 24 * 30) * 10)));
+
   }
 
   return (
     <div className="Prorata">
       <h1>Calulate Prorata !</h1>
 
+
+      <div className="date-entry" >
+
+        <DatePicker
+          selected={selectedDate2}
+          todayButton="Today"
+          onChange={date => setSelecetedDate2(date)}
+          dateFormat={'d-MM-yyyy'}
+          maxDate={new Date()}
+
+        />
+
+        <br />
+
+        <DatePicker
+          selected={selectedDate}
+          onChange={date => setSelecetedDate(date)}
+          dateFormat={'d-MM-yyyy'}
+          showYearDropdown
+          showMonthDropdown
+          maxDate={new Date()}
+
+        />
+
+        <h4>Battery has lasted {month} Months  </h4>
+
+      </div>
       <div className="number-inputs">
         <input
           type="number"
           value={number1}
           onChange={(e) => setNumber1(+e.target.value)}
           placeholder="Rate"
+          onBeforeInput={calculateMonth}
+
+
         />
-        <input
+        {/* <input
           type="number"
           value={number2}
           onChange={(e) => setNumber2(+e.target.value)}
           placeholder="Month"
-        />
+        /> */}
         <input
           type="number"
           value={number3}
           onChange={(e) => setNumber3(+e.target.value)}
           placeholder="Warranty"
+          onBeforeInput={calculateMonth}
         />
       </div>
 
-      <button onClick={calculateTotal}>Calculate!</button>
+      <button onClick={calculateTotal} onBeforeInput={calculateMonth}>Calculate!</button>
 
       <h2>{total}</h2>
       <div>Discount : {discount} %</div>
